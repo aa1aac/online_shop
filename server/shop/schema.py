40b76511e112c7a0898b4d3076d3288f7a71fa26
aa1aac <35446762+aa1aac@users.noreply.github.com>
2021-05-04 17:42:20 +0545

    create user authentication

---
+++ server/shop/schema.py
@@ -0,0 +1,30 @@
+import graphene
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
+
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+    password_change = mutations.PasswordChange.Field()
+    archive_account = mutations.ArchiveAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
+    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+    swap_emails = mutations.SwapEmails.Field()
+
+
+class Query(UserQuery, MeQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(AuthMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ server/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        blank=False, max_length=256, verbose_name='email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    is_seller = models.BooleanField(default=False)
